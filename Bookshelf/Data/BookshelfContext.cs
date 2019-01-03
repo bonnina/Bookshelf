a6c7e57c862a05aab045071dac4f44a999e9a5aa
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-03 22:33:44 +0200

    Create Books CRUD

---
+++ Bookshelf/Data/BookshelfContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf.Models
+{
+    public class BookshelfContext : DbContext
+    {
+        public BookshelfContext (DbContextOptions<BookshelfContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bookshelf.Models.Book> Book { get; set; }
+    }
+}
--- Bookshelf/Startup.cs
+++ Bookshelf/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Bookshelf.Models;
 
 namespace Bookshelf
 {
@@ -33,6 +35,9 @@ namespace Bookshelf
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<BookshelfContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BookshelfContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
