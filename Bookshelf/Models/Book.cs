767ffee1ea100ecc98237b403d8876de36a7c5f7
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-03 22:08:13 +0200

    Create Book model

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+    }
+}
