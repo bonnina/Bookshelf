3e96e9c4b7cc81ed61e17f5af6e002d29635bde6
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-05 18:22:13 +0200

    Create seed data

---
+++ Bookshelf/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Bookshelf.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookshelfContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<BookshelfContext>>()))
+            {
+                // Look for any books
+                if (context.Book.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Book.AddRange(
+                    new Book
+                    {
+                        Title = "Pollyannna",
+                        PublishDate = DateTime.Parse("1979-5-10"),
+                        Genre = "Fiction",
+                        Author = "Eleanor Potter"
+                    },
+
+                    new Book
+                    {
+                        Title = "The Complete Reference ",
+                        PublishDate = DateTime.Parse("2010-7-07"),
+                        Genre = "Education",
+                        Author = "Herbert Shildt"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
