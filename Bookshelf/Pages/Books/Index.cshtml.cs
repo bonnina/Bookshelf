ac2f6cf72673266325a7725566bc1a8821239f4a
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-11 21:41:18 +0200

    Add search by title and by genre

--- Bookshelf/Pages/Books/Index.cshtml.cs
+++ Bookshelf/Pages/Books/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Bookshelf.Models;
 
@@ -19,9 +20,32 @@ namespace Bookshelf.Pages.Books
         }
 
         public IList<Book> Book { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookGenre { get; set; }
 
         public async Task OnGetAsync()
         {
+            IQueryable<string> genreQuery = from b in _context.Book
+                                            orderby b.Genre
+                                            select b.Genre;
+
+            var books = from b in _context.Book
+                        select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BookGenre))
+            {
+                books = books.Where(x => x.Genre == BookGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Book = await _context.Book.ToListAsync();
         }
     }
