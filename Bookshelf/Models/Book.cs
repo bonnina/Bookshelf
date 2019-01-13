ab9f5c11de8483b48654c86cd8f2b7c8c7094a5a
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-13 17:31:00 +0200

    Add input validation

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -6,13 +6,28 @@ namespace Bookshelf.Models
     public class Book
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Publish Date")]
         [DataType(DataType.Date)]
         public DateTime PublishDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Author { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
